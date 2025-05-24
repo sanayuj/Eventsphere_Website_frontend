75421ec3079bf15214d5da5e910baaeec321d92a
sanayuj <sanayuj2255@gmail.com>
2025-05-25 00:05:32 +0530

    landing page completed

--- src/Components/User/LandingSection/LandingSection.jsx
+++ src/Components/User/LandingSection/LandingSection.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import "./LandingSection.css"
 function LandingSection() {
   return (
-    <div class="landingCommon d-flex justify-content-center align-items-center">
-    <h1 class="WelcomeNote text-center">Welcome to EventSphere</h1></div>
+    <div class="landingCommon d-flex flex-column justify-content-center align-items-center">
+    <h1 class="WelcomeNote text-center">Welcome to EventSphere</h1>
+    <p class="subheading">Shaping Moments, Crafting Experience!</p>
+    <div><button class="getStartedBtn">Get Started</button><button class="contactUsBtn">Contact Us</button></div>
+
+    </div>
   )
 }
 
