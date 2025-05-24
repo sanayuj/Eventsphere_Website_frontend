55d275c94eae075b3bdd7d36aad1d671ff0252f2
sanayuj <sanayuj2255@gmail.com>
2025-05-24 21:13:41 +0530

    navbar updated

--- src/Components/User/Navbar/Navbar.jsx
+++ src/Components/User/Navbar/Navbar.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
-      Heloo this is nav
-    </div>
-  )
-}
+    <nav className="navbar navbar-expand-lg customNav">
+      <div className="container-fluid">
+        <a className="navbar-brand heading ms-5" href="#">EventSphere</a>
 
-export default Navbar
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse " id="navbarNav">
+          <ul className="navbar-nav ms-auto gap-lg-4 me-5 ">
+            <li className="nav-item">
+              <a className="nav-link fs-7" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link fs-7" href="#">Service</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link fs-7" href="#">Events</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link fs-7" href="#">Team</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link fs-7" href="#">Hire</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link fs-7" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
