c1b189e3a874d378ffa7ea03f07578a093945f3c
sanayuj <sanayuj2255@gmail.com>
2025-05-25 00:13:22 +0530

    Service page started

---
+++ src/Components/User/Services/Services.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Services.css"
+function Services() {
+  return (
+    <div class="landingCommon  d-flex flex-column justify-content-center align-items-center">
+    <h2 className='ServiceHeading'>Our Services</h2>
+    
+    
+    </div>
+  )
+}
+
+export default Services
--- src/Pages/userPages/LandingPage.jsx
+++ src/Pages/userPages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../../Components/User/Navbar/Navbar'
 import LandingSection from '../../Components/User/LandingSection/LandingSection'
+import Services from '../../Components/User/Services/Services'
 
 function LandingPage() {
   return (
     <div><Navbar/>
     <LandingSection/>
+    <Services/>
     </div>
   )
 }
