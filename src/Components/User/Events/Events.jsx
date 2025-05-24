78f2c646cb798a5ae4df818ab7e9edb5e7989dd7
sanayuj <sanayuj2255@gmail.com>
2025-05-25 02:49:25 +0530

    Events

---
+++ src/Components/User/Events/Events.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Events.css"
+
+function Events() {
+  return (
+    <div className='landingSection3 d-flex flex-column justify-content-center align-items-center'>
+    <h3 className='EventsHeading'>Events</h3>
+    <div>
+     <div>
+       
+     </div>   
+    </div>
+    
+    </div>
+  )
+}
+
+export default Events
--- src/Pages/userPages/LandingPage.jsx
+++ src/Pages/userPages/LandingPage.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Navbar from '../../Components/User/Navbar/Navbar'
 import LandingSection from '../../Components/User/LandingSection/LandingSection'
 import Services from '../../Components/User/Services/Services'
+import Events from '../../Components/User/Events/Events'
 
 function LandingPage() {
   return (
     <div><Navbar/>
     <LandingSection/>
+    
+    <Events/>
     <Services/>
     </div>
   )
