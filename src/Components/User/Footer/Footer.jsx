7bbe2bf6f7520e2a18dcbb637ebe8499751d8ecc
Nakul Dev <nakuldev1561@gmail.com>
2025-05-28 21:27:21 +0530

    Refactor Contact and Footer components for improved readability; update CSS styles for better responsiveness and consistency

--- src/Components/User/Footer/Footer.jsx
+++ src/Components/User/Footer/Footer.jsx
@@ -5,13 +5,11 @@ function Footer() {
     <div> <nav className="navbar navbar-expand-lg customFooter">
       <div className="container-fluid">
         <a className="navbar-brand footer ms-5" href="#">&copy;
-     2025 EventSphere. All rights reserved.</a>
-
-        
+          2025 EventSphere. All rights reserved.</a>
 
         <div className="collapse navbar-collapse customFooter" id="navbarNav">
           <ul className="navbar-nav ms-auto gap-lg-4 me-5 ">
-             <li className="nav-item">
+            <li className="nav-item">
               <a className="nav-link fs-7 " href="#">Terms</a>
             </li>
             <li className="nav-item">
